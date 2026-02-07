953d751ea6a740825d028f8289713b8613f4aee5
KiraboM <kirabomusoke46@gmail.com>
2026-02-07 16:40:52 +0000

    Added functions to the javascript code

--- People-Counter-App/index.js
+++ People-Counter-App/index.js
@@ -1,7 +1,22 @@
 /* document.getElementById("Counter").innerText = "5"; */
+
+let countEl = document.getElementById("Counter")
+
+console.log(countEl)
 let count = 0
 function increment(){
     count += 1
+    countEl.innerText = count
     console.log(count)
 
+}
+
+function decrement(){
+    count -= 1
+    countEl.innerText = count
+    console.log(count)
+}
+
+function save(){
+    
 }
