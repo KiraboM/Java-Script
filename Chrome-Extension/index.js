7dee1e5ac3886d1ae34300800b5088a87ce3fc55
KiraboM <kirabomusoke46@gmail.com>
2026-02-11 19:26:14 +0000

    Initialised first page in chrome extension

---
+++ Chrome-Extension/index.js
@@ -0,0 +1,6 @@
+let myLeads = []
+const inputEl = document.getElementById("input")
+const inputBtn = document.getElementById("input-btn")
+inputBtn.addEventListener("click", function(){
+    console.log("Button clicked")
+})
