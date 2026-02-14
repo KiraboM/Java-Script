072898f66043dedf608f4c1d7c31c8ffea633e00
KiraboM <kirabomusoke46@gmail.com>
2026-02-14 23:30:05 +0000

    Completed first chrome extension!

--- Chrome-Extension/index.js
+++ Chrome-Extension/index.js
@@ -1,24 +1,54 @@
 let myLeads = []
+
 const inputEl = document.getElementById("input")
 const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-ulEl.innerText = ""
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+let ulEl = document.getElementById("ul-el")
+
+
+const leadsLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsLocalStorage && Array.isArray(leadsLocalStorage)){
+    myLeads = leadsLocalStorage
+    render(myLeads)
+}
+
 inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
+    if(inputEl.value){
+        myLeads.push(inputEl.value)
+        inputEl.value = ""
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    }
 })
 
-function renderLeads(){
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads) 
+    })
+})
+
+deleteBtn.addEventListener("dblclick", function(){
+    myLeads = []
+    localStorage.clear()
+    render(myLeads)
+    
+
+})
+
+function render(array){
     let listItems = ""
-    for(let i = 0; i < myLeads.length; i++){
-    //listItems +=  "<li><a href = " + myLeads[i] + " target = '_blank'>" + myLeads[i] + "</a></li>"
+    for(let i = 0; i < array.length; i++){
+    //listItems +=  "<li><a href = " + array[i] + " target = '_blank'>" + array[i] + "</a></li>"
     listItems +=  `
     <li>
-        <a href = '${myLeads[i]}' target = '_blank'>${myLeads[i]}</a>
+        <a href = '${array[i]}' target = '_blank'>${array[i]}</a>
     </li>`
   // const li = document.createElement("li")
-  // li.textContent = myLeads[i]
+  // li.textContent = array[i]
    //ulEl.append(li)
     }
     ulEl.innerHTML = listItems
