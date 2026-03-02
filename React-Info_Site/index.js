addb928999d0ac2cc3e67bf18c9148d4cedc6fca
KiraboM <kirabomusoke46@gmail.com>
2026-03-02 15:32:56 +0000

    Started on React Info Site

---
+++ React-Info_Site/index.js
@@ -0,0 +1,4 @@
+/* ReactDOM.render(<ul><li>Hi my name is Bob!</li><li>I like to play golf</li></ul>,
+                document.getElementById("root"))
+ */
+ReactDOM.render(<ul><li>Thing 1</li><li>Thing 2</li></ul>, document.getElementById("root"))
