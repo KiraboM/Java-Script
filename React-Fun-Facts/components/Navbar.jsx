d1184057f3ae339c9d8e11c9b630332a11af8f97
KiraboM <kirabomusoke46@gmail.com>
2026-03-11 21:26:31 +0000

    Practiced React and JSX

---
+++ React-Fun-Facts/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar(){
+    return(
+        <nav className="navbar">
+            <img src="./react.svg"></img>
+            <h1><span className="facts">React Facts</span><span className="course">React Course Project 1</span></h1>
+        </nav>
+    )
+}
